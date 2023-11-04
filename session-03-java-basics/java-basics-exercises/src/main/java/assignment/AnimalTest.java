eb6a2d11841bd5fd7581a5d1f0b205a2eaacde41
EnverJ <Enlem1987@gmail.com>
2023-11-03 21:25:23 -0600

    added new animals to AnimalTest class

--- session-03-java-basics/java-basics-exercises/src/main/java/assignment/AnimalTest.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/assignment/AnimalTest.java
@@ -1,10 +1,12 @@
 package assignment;
 
+import java.util.Arrays;
+
 public class AnimalTest {
     public static void main(String[] args) {
         Animal[] animals = new Animal[3];
         animals[0] = new Dog("Buddy", 5, "Golden Retriever");
-        animals[1] = new Cat("Whiskers", 3,20);
+        animals[1] = new Cat("Whiskers", 3, 20);
         animals[2] = new Animal("Mystery Animal", 7);
 
         for (Animal animal : animals) {
