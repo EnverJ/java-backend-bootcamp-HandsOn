9600ec486e3b847d4eaa1e2094f6bfe247ce2e85
EnverJ <Enlem1987@gmail.com>
2023-11-07 21:43:55 -0700

    added data types

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
@@ -5,20 +5,21 @@ public class JavaVariables {
     public static void main(String[] args) {
         // Step 1: Declare and initialize variables of different data types
         // int variable to store an integer value
+        int a;
 
         // long variable to store a long value
-
+         long distance = 1000000L;
         // float variable to store a floating-point value
-
+        float myFloatVariable = 111.334f;
 
         // double variable to store a double value
-
+        double averageSalary = 2344.2342334523432;
 
         // char variable to store a single character
-
+          char letter = 'a';
 
         // boolean variable to store a boolean value
-
+     boolean isValid = true;
 
         // String variable to store a string of characters
 
